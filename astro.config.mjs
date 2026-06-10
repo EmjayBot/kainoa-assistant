7197a81462f8e1d4d4843c2c0a6dcbc05f00914b
EmjayBot <71861583+EmjayBot@users.noreply.github.com>
2026-06-10 04:00:09 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://emjaybot.github.io',
   base: '/kainoa-assistant/',
   integrations: [
+    react(),
     starlight({
       title: 'Kainoa Assistant',
       social: {
